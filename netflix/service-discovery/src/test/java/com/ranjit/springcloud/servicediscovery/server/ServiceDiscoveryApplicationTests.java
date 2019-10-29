5a94110164eb6addf1c17d836006191212ddced7
Ranjit Patra <mailtoranjitpatra@gmail.com>
2019-10-29 17:54:06 +0530

    Added Discovery Service

---
+++ netflix/service-discovery/src/test/java/com/ranjit/springcloud/servicediscovery/server/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjit.springcloud.servicediscovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
